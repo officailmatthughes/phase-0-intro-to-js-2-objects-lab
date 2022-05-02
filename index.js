1654229d30cb40b85e63951e2d2b215c01993ecf
Mattherw <officialmatthughes>
2022-05-01 18:33:43 -0700

    commit tree fa8a38cf1744e36966eb679151b22496f1541227

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = new Object()
+employee.name = "Larry";
+employee.streetAddress = "1 market st";
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const new_employee = {...employee}
+    new_employee[key] = value;  
+
+    return new_employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const new_employee = employee;
+    new_employee[key] = value;
+
+    return new_employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const new_employee = {...employee}
+    delete new_employee[key]
+
+    return new_employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const new_employee = employee
+    delete new_employee[key]
+
+    return new_employee;
+}
